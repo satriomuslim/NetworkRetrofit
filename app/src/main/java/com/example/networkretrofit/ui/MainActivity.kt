628824c1de95d8885de6b3648b9abaf9fd3f0788
satriomuslim <satriomuslim2627@gmail.com>
2023-06-08 13:15:42 +0700

    show retrofit list

--- app/src/main/java/com/example/networkretrofit/ui/MainActivity.kt
+++ app/src/main/java/com/example/networkretrofit/ui/MainActivity.kt
@@ -2,11 +2,103 @@ package com.example.networkretrofit.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.networkretrofit.R
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.networkretrofit.data.response.CustomerReviewsItem
+import com.example.networkretrofit.data.response.NetworkResponse
+import com.example.networkretrofit.data.response.Restaurant
+import com.example.networkretrofit.data.retrofit.ApiConfig
+import com.example.networkretrofit.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.http.Tag
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvReview.layoutManager = layoutManager
+
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.rvReview.addItemDecoration(itemDecoration)
+
+        findRestaurant()
+
+    }
+
+    private fun findRestaurant() {
+        showLoading(true)
+
+        val client = ApiConfig.getApiService().getNetwork(RESTAURANT_ID)
+        client.enqueue(object : Callback<NetworkResponse> {
+            override fun onResponse(
+                call: Call<NetworkResponse>,
+                response: Response<NetworkResponse>,
+            ) {
+               showLoading(false)
+               if (response.isSuccessful) {
+                   val responseBody = response.body()
+                        if (responseBody != null) {
+                            setNetworkData(responseBody.restaurant)
+                            setReviewData(responseBody.restaurant.customerReviews)
+                        }
+                   } else {
+                       Log.e(TAG, "onFailure: ${response.message()}")
+
+                   }
+               }
+
+
+            override fun onFailure(call: Call<NetworkResponse>, t: Throwable) {
+                showLoading(false)
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+        
+    }
+
+    private fun setNetworkData(restaurant: Restaurant) {
+        binding.tvTitle.text = restaurant.name
+        binding.tvDescription.text = restaurant.description
+
+        Glide.with(this@MainActivity)
+            .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+            .into(binding.ivPicture)
+    }
+
+    private fun setReviewData(customerReviews: List<CustomerReviewsItem>) {
+        val adapter = ReviewAdapter()
+        adapter.submitList(customerReviews)
+
+        binding.rvReview.adapter = adapter
+        binding.edReview.setText("")
+
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/networkretrofit/ui/ReviewAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.networkretrofit.ui
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.example.networkretrofit.data.response.CustomerReviewsItem
+import com.example.networkretrofit.databinding.ItemReviewBinding
+
+
+class ReviewAdapter : ListAdapter<CustomerReviewsItem, ReviewAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ItemReviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val review = getItem(position)
+        holder.bind(review)
+    }
+
+    class MyViewHolder(private val binding: ItemReviewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(review: CustomerReviewsItem){
+            binding.tvItem.text = "${review.review}\n- ${review.name}"
+        }
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<CustomerReviewsItem>() {
+            override fun areItemsTheSame(oldItem: CustomerReviewsItem, newItem: CustomerReviewsItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: CustomerReviewsItem, newItem: CustomerReviewsItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+}
