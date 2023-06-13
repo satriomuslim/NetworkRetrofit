37b90b6a52d03cde1a38cd293c94eee8a683528d
satriomuslim <satriomuslim2627@gmail.com>
2023-06-13 14:44:34 +0700

    single event

--- app/src/main/java/com/example/networkretrofit/ui/MainActivity.kt
+++ app/src/main/java/com/example/networkretrofit/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.networkretrofit.ui
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.lifecycle.ViewModelProvider
@@ -11,23 +10,13 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.networkretrofit.data.response.CustomerReviewsItem
-import com.example.networkretrofit.data.response.NetworkResponse
-import com.example.networkretrofit.data.response.PostReviewResponse
 import com.example.networkretrofit.data.response.Restaurant
-import com.example.networkretrofit.data.retrofit.ApiConfig
 import com.example.networkretrofit.databinding.ActivityMainBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    companion object {
-        private const val TAG = "MainActivity"
-        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,6 +44,15 @@ class MainActivity : AppCompatActivity() {
             showLoading(it)
         }
 
+        mainViewModel.snackbarText.observe(this) {
+            it.getContentIfNotHandled()?.let { snackBarText ->
+                Snackbar.make(
+                    window.decorView.rootView,
+                    snackBarText,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
 
         binding.btnSend.setOnClickListener { view ->
             mainViewModel.postReview(binding.edReview.text.toString())
@@ -80,7 +78,6 @@ class MainActivity : AppCompatActivity() {
 
         binding.rvReview.adapter = adapter
         binding.edReview.setText("")
-
     }
 
     private fun showLoading(isLoading: Boolean) {
